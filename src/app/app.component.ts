7b404d957d2420ce4b3f42f3eded97810a7ff9d5
Maicon Pinto <maiconsilva.pinto@gmail.com>
2017-02-18 18:21:34 -0200

    Menu update. Dynamic list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, MenuController } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
@@ -11,12 +11,24 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage = HomePage;
 
-  constructor(platform: Platform) {
+  pages: Array<{title: string, component: any}> = [];
+
+  constructor(platform: Platform, public menuCtrl: MenuController) {
+
+    this.pages = [
+      {title: 'Home', component: HomePage}
+    ];
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
+  }
+
+  openPage(page: {title: string, component: any}): void {
+    this.rootPage = page.component;
+    this.menuCtrl.close();
   }
 }
