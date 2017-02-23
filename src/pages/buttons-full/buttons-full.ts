a09688f164799fce5e52cbb3c70297dd7081b0d6
Maicon Pinto <maiconsilva.pinto@gmail.com>
2017-02-23 09:32:04 -0300

    Buttons Full

---
+++ src/pages/buttons-full/buttons-full.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-buttons-full',
+  templateUrl: 'buttons-full.html'
+})
+export class ButtonsFullPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+}
