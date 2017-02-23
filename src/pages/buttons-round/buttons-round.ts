a93e881ba886492284cedf26541b0015e84c004f
Maicon Pinto <maiconsilva.pinto@gmail.com>
2017-02-23 09:31:25 -0300

    Buttons Round

---
+++ src/pages/buttons-round/buttons-round.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-buttons-round',
+  templateUrl: 'buttons-round.html'
+})
+export class ButtonsRoundPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+}
