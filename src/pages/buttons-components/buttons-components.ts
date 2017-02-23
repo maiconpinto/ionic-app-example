3ef58ed8cff3f1c7b4991342092495b2c4bd5e72
Maicon Pinto <maiconsilva.pinto@gmail.com>
2017-02-23 09:32:54 -0300

    Buttons Components

---
+++ src/pages/buttons-components/buttons-components.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-buttons-components',
+  templateUrl: 'buttons-components.html'
+})
+export class ButtonsComponentsPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+}
