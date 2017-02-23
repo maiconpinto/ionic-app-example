b5560f22b5171c282f142594793ce67fa406f92f
Maicon Pinto <maiconsilva.pinto@gmail.com>
2017-02-23 09:31:08 -0300

    Buttons Clear

---
+++ src/pages/buttons-clear/buttons-clear.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-buttons-clear',
+  templateUrl: 'buttons-clear.html'
+})
+export class ButtonsClearPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+}
