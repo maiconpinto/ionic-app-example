d4f84cf62042d8de83bcb4b9b9e44c21ad53908d
Maicon Pinto <maiconsilva.pinto@gmail.com>
2017-02-23 09:32:34 -0300

    Buttons Icons

---
+++ src/pages/buttons-icons/buttons-icons.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-buttons-icons',
+  templateUrl: 'buttons-icons.html'
+})
+export class ButtonsIconsPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+}
