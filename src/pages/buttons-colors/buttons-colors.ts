ce9c21176175cd74f88e355df9ca803e1f833c2e
Maicon Pinto <maiconsilva.pinto@gmail.com>
2017-02-23 09:30:20 -0300

    Buttons Colors

---
+++ src/pages/buttons-colors/buttons-colors.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-buttons-colors',
+  templateUrl: 'buttons-colors.html'
+})
+export class ButtonsColorsPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+}
