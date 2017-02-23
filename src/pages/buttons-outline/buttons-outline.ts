a368212a41ab47dcc8d35896e944a14485fb40bf
Maicon Pinto <maiconsilva.pinto@gmail.com>
2017-02-23 09:30:50 -0300

    Buttons Outline

---
+++ src/pages/buttons-outline/buttons-outline.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-buttons-outline',
+  templateUrl: 'buttons-outline.html'
+})
+export class ButtonsOutlinePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+}
