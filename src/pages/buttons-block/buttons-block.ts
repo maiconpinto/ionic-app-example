566d6ff4d1cb33d3d4c5162cac6b5a229c1f4b46
Maicon Pinto <maiconsilva.pinto@gmail.com>
2017-02-23 09:31:48 -0300

    Buttons Block

---
+++ src/pages/buttons-block/buttons-block.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-buttons-block',
+  templateUrl: 'buttons-block.html'
+})
+export class ButtonsBlockPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+}
