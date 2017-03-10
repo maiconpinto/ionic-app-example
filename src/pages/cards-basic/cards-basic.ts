334ee0513f9c740de2450ba0c05c8e54a6fd7cde
Maicon Pinto <maiconsilva.pinto@gmail.com>
2017-03-10 03:26:38 -0300

    Basic Cards

---
+++ src/pages/cards-basic/cards-basic.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-cards-basic',
+  templateUrl: 'cards-basic.html'
+})
+export class CardsBasicPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+}
