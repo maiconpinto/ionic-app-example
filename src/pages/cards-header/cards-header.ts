dc1b890606afcbc566959e2fa5900a64bb99ad04
Maicon Pinto <maiconsilva.pinto@gmail.com>
2017-03-10 03:27:01 -0300

    Header Cards

---
+++ src/pages/cards-header/cards-header.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-cards-header',
+  templateUrl: 'cards-header.html'
+})
+export class CardsHeaderPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+}
