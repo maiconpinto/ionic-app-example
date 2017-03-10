90ab9f9c131410b0a5890587fedf067980f6fbdf
Maicon Pinto <maiconsilva.pinto@gmail.com>
2017-03-10 03:27:55 -0300

    Images Cards

---
+++ src/pages/cards-images/cards-images.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-cards-images',
+  templateUrl: 'cards-images.html'
+})
+export class CardsImagesPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+}
