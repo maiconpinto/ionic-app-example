8d3936585e0a2a2dda526828d2c1b1b420fef9a3
Maicon Pinto <maiconsilva.pinto@gmail.com>
2017-03-10 03:28:30 -0300

    Social Cards

---
+++ src/pages/cards-social/cards-social.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-cards-social',
+  templateUrl: 'cards-social.html'
+})
+export class CardsSocialPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+}
