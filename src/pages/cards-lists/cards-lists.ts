2adb01d7e8090a97a5791f5dbd1c4ca1aee07264
Maicon Pinto <maiconsilva.pinto@gmail.com>
2017-03-10 03:27:28 -0300

    Lists Cards

---
+++ src/pages/cards-lists/cards-lists.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-cards-lists',
+  templateUrl: 'cards-lists.html'
+})
+export class CardsListsPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+}
