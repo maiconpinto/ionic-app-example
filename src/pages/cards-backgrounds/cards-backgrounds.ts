29fc1dfecbad1b57ab271e4afbae664fe7d6df67
Maicon Pinto <maiconsilva.pinto@gmail.com>
2017-03-10 03:28:13 -0300

    Backgrounds Cards

---
+++ src/pages/cards-backgrounds/cards-backgrounds.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-cards-backgrounds',
+  templateUrl: 'cards-backgrounds.html'
+})
+export class CardsBackgroundsPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+}
