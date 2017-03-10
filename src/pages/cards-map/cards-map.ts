6e586fd724d11ea6db4d10c2f89fa7f7ae08e38c
Maicon Pinto <maiconsilva.pinto@gmail.com>
2017-03-10 03:28:53 -0300

    Map Cards

---
+++ src/pages/cards-map/cards-map.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-cards-map',
+  templateUrl: 'cards-map.html'
+})
+export class CardsMapPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+}
